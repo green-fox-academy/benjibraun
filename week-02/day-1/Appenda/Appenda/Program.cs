6f5703854d7589b9098144a80e7142d7dc7a19fd
benjibraun <benjibraun@gmail.com>
2018-02-22 12:24:13 +0100

    spaces between the for loop parameters added

--- week-02/day-1/Appenda/Appenda/Program.cs
+++ week-02/day-1/Appenda/Appenda/Program.cs
@@ -12,7 +12,7 @@ namespace Appenda
             // - Add all elements an `"a"` at the end
 
             string[] nimals = { "kuty", "macsk", "cic" };
-            for (int i=0;i<nimals.Length;i++)
+            for (int i=0; i<nimals.Length; i++)
             {
                 nimals[i] = nimals[i] + "a";
                 
