a464b0f8c7fd143ef94cd13a1d22d1712913242a
benjibraun <benjibraun@gmail.com>
2018-02-23 11:34:25 +0100

    gitignore,week2

---
+++ week-02/day-3/Tothecenter/Tothecenter/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tothecenter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // Create a line drawing function that takes 2 parameters:
+            // The x and y coordinates of the line's starting point
+            // and draws a line from that point to the center of the canvas.
+            // Draw 3 lines with that function. Use loop for that.
+
+            for (double i = 0; i < 3; i++)
+            {
+            LineDrawing(foxDraw,i*100, i*50);
+
+            }
+            
+        }
+        public static void LineDrawing (FoxDraw foxDraw, double  x,double y)
+        {
+            foxDraw.DrawLine(x, y, 250, 250);
+        }
+    }
+
+}
---
+++ week-02/day-3/Triangles/Triangles/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Triangles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            double center = canvas.Width / 2;
+            DrawTriangle(foxDraw, canvas, 50);
+        }
+        public static void DrawTriangle(FoxDraw foxDraw,Canvas canvas, double center)
+        {
+            
+            double root = (System.Math.Pow(20, (1.0 / 3.0)));
+            double h = (Math.Sqrt(3) / 2) * 20;
+            foxDraw.DrawLine(center, 0, center + 10, h);
+            foxDraw.DrawLine(center, 0, center -10, h);
+            foxDraw.DrawLine(center-10, h, center + 10, h);
+
+        }
+    }
+}
