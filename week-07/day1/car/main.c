5f62f261b26e134655974d18e33eb912e230b8e8
benjibraun <benjibraun@gmail.com>
2018-04-03 14:03:03 +0200

    car

--- week-07/day1/car/main.c
+++ week-07/day1/car/main.c
@@ -3,16 +3,30 @@
 // Write a function that takes a car as an argument and prints all it's stats
 // If the car is a Tesla it should not print it's gas level
 
-enum car_type {VOLVO, TOYOTA, LAND_ROVER, TESLA};
+typedef enum{VOLVO, TOYOTA, LAND_ROVER, TESLA}car_type;
 
-struct car {
-	enum car_type type;
+typedef struct{
+	car_type type;
 	double km;
 	double gas;
-};
-
+}car;
+void print_stats(car car);
 int main()
 {
-
+    car volovo={VOLVO,35247.5,45.2};
+    car tesla={TESLA,19438.4};
+    print_stats(volovo);
+    print_stats(tesla);
 	return 0;
+}
+void print_stats(car car)
+{
+    if(car.type==TESLA)
+    {
+        printf("%d %.1f\n",car.type,car.km);
+    }
+    else
+    {
+        printf("%d %.1f %.1f\n",car.type,car.km,car.gas);
+    }
 }
