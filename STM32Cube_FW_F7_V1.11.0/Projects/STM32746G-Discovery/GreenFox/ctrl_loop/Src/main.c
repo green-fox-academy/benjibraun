8f21f905e938725f1141ade3fa9e8a3871f59bea
benjibraun <benjibraun@gmail.com>
2018-04-27 15:45:17 +0200

    ADC controll

--- STM32Cube_FW_F7_V1.11.0/Projects/STM32746G-Discovery/GreenFox/ctrl_loop/Src/main.c
+++ STM32Cube_FW_F7_V1.11.0/Projects/STM32746G-Discovery/GreenFox/ctrl_loop/Src/main.c
@@ -75,12 +75,16 @@ input_capture_data_t inputc;
 int freq;
 float T_period;
 int steps;
+int semp[10];
+int avg;
+int cnt = 0;
+int avg_cnt = 0;
 /* Private function prototypes -----------------------------------------------*/
 static void SystemClock_Config(void);
 static void Error_Handler(void);
 static void MPU_Config(void);
 static void CPU_CACHE_Enable(void);
-void periphera_init (void);
+void periphera_init(void);
 void LCD_print();
 
 /* Private functions ---------------------------------------------------------*/
@@ -90,7 +94,7 @@ void LCD_print();
  * @retval None
  */
 
-		uint16_t analog_read;
+uint16_t analog_read;
 
 int main(void) {
 	/* This project template calls firstly two functions in order to configure MPU feature
@@ -117,7 +121,7 @@ int main(void) {
 	SystemClock_Config();
 
 	/* Add your application code here */
-	periphera_init ();
+	periphera_init();
 
 	int8_t cntr = 0;
 	TIM3->CCR1 = 50;
@@ -127,68 +131,84 @@ int main(void) {
 	p_init(&ctrler);
 	pi_init(&ctrler_pi);
 	pid_init(&ctrler_pid);
+	for (int i = 0; i < 10; i++) {
+		semp[i] = -1;
+	}
+
 	/* Infinite loop */
 	while (1) {
-		char buff[100];
-		char buff2[100];
 		HAL_ADC_Start(&AdcHandle);
-		if (HAL_ADC_PollForConversion(&AdcHandle, 10) == HAL_OK)
-			  {
-			    // ADC conversion completed
-			analog_read = HAL_ADC_GetValue(&AdcHandle);
-			  }
-		cntr++;
-
-		LCD_print();
-		ctrler_pi.sense = freq/4;
-		TIM3->CCR1 = (int) pi_control(&ctrler_pi);
-		HAL_Delay(10);
+		avg = 0;
+		avg_cnt = 0;
+		if (HAL_ADC_PollForConversion(&AdcHandle, 10) == HAL_OK) {
+			// ADC conversion completed
+			analog_read = HAL_ADC_GetValue(&AdcHandle) / 40.95;
+			if (cnt == 9) {
+				semp[cnt] = analog_read;
+				cnt = 0;
+			} else {
+				semp[cnt] = analog_read;
+				cnt++;
+			}
+			for (int i = 0; i < 10; i++) {
+				if (semp[i] != -1) {
+				avg += semp[i];
+				avg_cnt++;
+			}
+			avg = avg/2;
+			TIM3->CCR1 = avg;
+		}
 	}
+	cntr++;
+
+	LCD_print();
+	ctrler_pi.sense = freq / 4;
+	//TIM3->CCR1 = (int) pi_control(&ctrler_pi);
+	HAL_Delay(10);
+}
 }
 
 void LCD_print() {
-	char buff[100];
+char buff[100];
 
+sprintf(buff, "%d", TIM3->CCR1);
+BSP_LCD_ClearStringLine(0);
+BSP_LCD_DisplayStringAtLine(0, (uint8_t *) buff);
 
-	sprintf(buff, "%d", TIM3->CCR1);
-	BSP_LCD_ClearStringLine(0);
-	BSP_LCD_DisplayStringAtLine(0, (uint8_t *) buff);
+sprintf(buff, "%d", avg);
+BSP_LCD_ClearStringLine(1);
+BSP_LCD_DisplayStringAtLine(1, (uint8_t *) buff);
 
-	sprintf(buff, "%d", analog_read);
-	BSP_LCD_ClearStringLine(1);
-	BSP_LCD_DisplayStringAtLine(1, (uint8_t *) buff);
+sprintf(buff, "%d", avg_cnt);
+BSP_LCD_ClearStringLine(2);
+BSP_LCD_DisplayStringAtLine(2, (uint8_t *) buff);
 
-	sprintf(buff, "%f", ctrler_pi.sense);
-	BSP_LCD_ClearStringLine(2);
-	BSP_LCD_DisplayStringAtLine(2, (uint8_t *) buff);
-
-	sprintf(buff, "%d", freq);
-	BSP_LCD_ClearStringLine(3);
-	BSP_LCD_DisplayStringAtLine(3, (uint8_t *) buff);
-	HAL_Delay(100);
+sprintf(buff, "%d", freq);
+BSP_LCD_ClearStringLine(3);
+BSP_LCD_DisplayStringAtLine(3, (uint8_t *) buff);
+HAL_Delay(100);
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-	inputc.ovf++;
+inputc.ovf++;
 }
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
-	char buff[100];
-	inputc.prev = inputc.last;
-	inputc.last = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
-	steps = (65535 * inputc.ovf) + inputc.last - inputc.prev;
-	T_period = (float)((inputc.ovf * IC_PERIOD + inputc.last - inputc.prev) * 0.001);
-	freq = (1 / T_period) * 1000;
-	inputc.ovf = 0;
-	//BSP_LED_Toggle(LED_GREEN);
+char buff[100];
+inputc.prev = inputc.last;
+inputc.last = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+steps = (65535 * inputc.ovf) + inputc.last - inputc.prev;
+T_period = (float) ((inputc.ovf * IC_PERIOD + inputc.last - inputc.prev) * 0.001);
+freq = (1 / T_period) * 1000;
+inputc.ovf = 0;
+//BSP_LED_Toggle(LED_GREEN);
 }
 
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* AdcHandle)
-{
-  /* Turn LED1 on: Transfer process is correct */
-  BSP_LED_On(LED1);
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* AdcHandle) {
+/* Turn LED1 on: Transfer process is correct */
+BSP_LED_On(LED1);
 }
 
-void periphera_init (void){
+void periphera_init(void) {
 BSP_LCD_Init();
 BSP_LCD_LayerDefaultInit(0, LCD_FB_START_ADDRESS);
 BSP_LCD_SelectLayer(0);
@@ -198,11 +218,16 @@ BSP_LED_Init(LED_GREEN);
 
 /* time + PWM config */
 
-__HAL_RCC_TIM2_CLK_ENABLE();
-__HAL_RCC_TIM3_CLK_ENABLE();
-__HAL_RCC_GPIOA_CLK_ENABLE();
-__HAL_RCC_GPIOB_CLK_ENABLE();
-__HAL_RCC_ADC3_CLK_ENABLE();
+__HAL_RCC_TIM2_CLK_ENABLE()
+;
+__HAL_RCC_TIM3_CLK_ENABLE()
+;
+__HAL_RCC_GPIOA_CLK_ENABLE()
+;
+__HAL_RCC_GPIOB_CLK_ENABLE()
+;
+__HAL_RCC_ADC3_CLK_ENABLE()
+;
 
 uart_handle.Init.BaudRate = 115200;
 uart_handle.Init.WordLength = UART_WORDLENGTH_8B;
@@ -278,20 +303,19 @@ gpio2.Speed = GPIO_SPEED_HIGH;
 
 HAL_GPIO_Init(GPIOA, &gpio2);
 
-
-AdcHandle.Instance          		 = ADC3;
-AdcHandle.Init.ClockPrescaler        = ADC_CLOCKPRESCALER_PCLK_DIV4;
-AdcHandle.Init.Resolution            = ADC_RESOLUTION_12B;
-AdcHandle.Init.ScanConvMode          = DISABLE;                       // Sequencer disabled (ADC conversion on only 1 channel: channel set on rank 1)
-AdcHandle.Init.ContinuousConvMode    = DISABLE;                       // Continuous mode enabled to have continuous conversion
+AdcHandle.Instance = ADC3;
+AdcHandle.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV4;
+AdcHandle.Init.Resolution = ADC_RESOLUTION_12B;
+AdcHandle.Init.ScanConvMode = DISABLE;                       // Sequencer disabled (ADC conversion on only 1 channel: channel set on rank 1)
+AdcHandle.Init.ContinuousConvMode = DISABLE;                       // Continuous mode enabled to have continuous conversion
 AdcHandle.Init.DiscontinuousConvMode = DISABLE;                       // Parameter discarded because sequencer is disabled
-AdcHandle.Init.NbrOfDiscConversion   = 0;
-AdcHandle.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_NONE; // Conversion start trigged at each external event
-AdcHandle.Init.ExternalTrigConv      = ADC_EXTERNALTRIGCONV_T1_CC1;
-AdcHandle.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
-AdcHandle.Init.NbrOfConversion       = 1;
+AdcHandle.Init.NbrOfDiscConversion = 0;
+AdcHandle.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE; // Conversion start trigged at each external event
+AdcHandle.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T1_CC1;
+AdcHandle.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+AdcHandle.Init.NbrOfConversion = 1;
 AdcHandle.Init.DMAContinuousRequests = DISABLE;
-AdcHandle.Init.EOCSelection          = DISABLE;
+AdcHandle.Init.EOCSelection = DISABLE;
 
 HAL_ADC_Init(&AdcHandle);
 
@@ -303,7 +327,6 @@ sADCConfig.Offset = 0;
 HAL_ADC_ConfigChannel(&AdcHandle, &sADCConfig);
 HAL_ADC_Start(&AdcHandle);
 }
-
 
 /**
  * @brief  System Clock Configuration
@@ -326,38 +349,38 @@ HAL_ADC_Start(&AdcHandle);
  * @retval None
  */
 static void SystemClock_Config(void) {
-	RCC_ClkInitTypeDef RCC_ClkInitStruct;
-	RCC_OscInitTypeDef RCC_OscInitStruct;
+RCC_ClkInitTypeDef RCC_ClkInitStruct;
+RCC_OscInitTypeDef RCC_OscInitStruct;
 
-	/* Enable HSE Oscillator and activate PLL with HSE as source */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLM = 25;
-	RCC_OscInitStruct.PLL.PLLN = 432;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-	RCC_OscInitStruct.PLL.PLLQ = 9;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-		Error_Handler();
-	}
+/* Enable HSE Oscillator and activate PLL with HSE as source */
+RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+RCC_OscInitStruct.PLL.PLLM = 25;
+RCC_OscInitStruct.PLL.PLLN = 432;
+RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+RCC_OscInitStruct.PLL.PLLQ = 9;
+if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+	Error_Handler();
+}
 
-	/* activate the OverDrive to reach the 216 Mhz Frequency */
-	if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
-		Error_Handler();
-	}
+/* activate the OverDrive to reach the 216 Mhz Frequency */
+if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
+	Error_Handler();
+}
 
-	/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
-	 clocks dividers */
-	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV8;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK) {
-		Error_Handler();
-	}
+/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+ clocks dividers */
+RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV8;
+RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK) {
+	Error_Handler();
+}
 }
 
 /**
@@ -366,9 +389,9 @@ static void SystemClock_Config(void) {
  * @retval None
  */
 static void Error_Handler(void) {
-	/* User may add here some code to deal with this error */
-	while (1) {
-	}
+/* User may add here some code to deal with this error */
+while (1) {
+}
 }
 
 /**
@@ -379,28 +402,28 @@ static void Error_Handler(void) {
  * @retval None
  */
 static void MPU_Config(void) {
-	MPU_Region_InitTypeDef MPU_InitStruct;
+MPU_Region_InitTypeDef MPU_InitStruct;
 
-	/* Disable the MPU */
-	HAL_MPU_Disable();
+/* Disable the MPU */
+HAL_MPU_Disable();
 
-	/* Configure the MPU attributes as WT for SRAM */
-	MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-	MPU_InitStruct.BaseAddress = 0x20010000;
-	MPU_InitStruct.Size = MPU_REGION_SIZE_256KB;
-	MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
-	MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
-	MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
-	MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
-	MPU_InitStruct.Number = MPU_REGION_NUMBER0;
-	MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-	MPU_InitStruct.SubRegionDisable = 0x00;
-	MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+/* Configure the MPU attributes as WT for SRAM */
+MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+MPU_InitStruct.BaseAddress = 0x20010000;
+MPU_InitStruct.Size = MPU_REGION_SIZE_256KB;
+MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+MPU_InitStruct.SubRegionDisable = 0x00;
+MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
 
-	HAL_MPU_ConfigRegion(&MPU_InitStruct);
+HAL_MPU_ConfigRegion(&MPU_InitStruct);
 
-	/* Enable the MPU */
-	HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+/* Enable the MPU */
+HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
 }
 
 /**
@@ -409,11 +432,11 @@ static void MPU_Config(void) {
  * @retval None
  */
 static void CPU_CACHE_Enable(void) {
-	/* Enable I-Cache */
-	SCB_EnableICache();
+/* Enable I-Cache */
+SCB_EnableICache();
 
-	/* Enable D-Cache */
-	SCB_EnableDCache();
+/* Enable D-Cache */
+SCB_EnableDCache();
 }
 
 #ifdef  USE_FULL_ASSERT
@@ -427,13 +450,13 @@ static void CPU_CACHE_Enable(void) {
  */
 void assert_failed(uint8_t* file, uint32_t line)
 {
-	/* User can add his own implementation to report the file name and line number,
-	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+/* User can add his own implementation to report the file name and line number,
+ ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
-	/* Infinite loop */
-	while (1)
-	{
-	}
+/* Infinite loop */
+while (1)
+{
+}
 }
 #endif
 
